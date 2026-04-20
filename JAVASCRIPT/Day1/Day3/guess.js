a659a01c9dc523b2ba4635c496dfef0ed0f9ce1b
DivyanshMishra06 <mishradm705@gmail.com>
2026-04-20 12:39:38 +0530

    Remove unnecessary frontend practice folders

--- JAVASCRIPT/Day1/Day3/guess.js
+++
@@ -1,6 +0,0 @@
-let gameNum=25;
-let userNum=prompt("Guess the game number:");
-while(userNum!=gameNum){
-    userNum=prompt("you entered wrong number,Try Again:");
-}
-console.log("congratulations, you entered the right number");
--- JAVASCRIPT/Day1/Day4/pr2.js
+++
@@ -1,5 +0,0 @@
-let company=["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
-let arr=company.shift();
-console.log(company);
-company.splice(2,0,"ola");
-company.push("Amazon");
